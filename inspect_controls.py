441289376eaf91f6b8a12a104e5c28b60d0dd36c
jwhicks727 <jwhicks727@gmail.com>
2026-04-20 15:11:09 -0700

    Delete inspect_controls.py

--- inspect_controls.py
+++
@@ -1,7 +0,0 @@
-from pywinauto import Application
-
-app = Application(backend='uia').connect(
-    title='Valcom Utility Program Ver. 3.8.0.0'
-)
-window = app.window(title='Valcom Utility Program Ver. 3.8.0.0')
-window.print_control_identifiers()
